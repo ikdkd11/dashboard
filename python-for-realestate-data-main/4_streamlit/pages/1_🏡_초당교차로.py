1b4e608fc2be0e5dc5c4511262ece68666349962
ikdkd11 <kdkime86@gmail.com>
2024-03-20 16:38:16 +0900

    Update 1_🏡_초당교차로.py

--- python-for-realestate-data-main/4_streamlit/pages/1_🏡_초당교차로.py
+++ python-for-realestate-data-main/4_streamlit/pages/1_🏡_초당교차로.py
@@ -67,9 +67,9 @@ col1, col2 = st.columns([1,1])
 with col1:
     st.plotly_chart(grph1, use_container_width = True)
 with col2:
-    st.subheader('위험구간 중 최저 노면온도 기록구간 진입 전/후 평균 노면온도 비교')
     tab1, tab2 = st.tabs(["Table", "Graph"])
     with tab1:
+       st.subheader('위험구간 중 최저 노면온도 기록구간 진입 전/후 평균 노면온도 비교')
        st.table(table1.head(5))
     with tab2:
         st.plotly_chart(box11, use_container_width=True)
