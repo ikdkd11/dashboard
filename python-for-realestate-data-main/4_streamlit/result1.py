d3ead8ca5baf81ea956e01d6653924569a0009eb
ikdkd11 <kdkime86@gmail.com>
2024-03-26 13:13:41 +0900

    12

--- python-for-realestate-data-main/4_streamlit/result1.py
+++ python-for-realestate-data-main/4_streamlit/result1.py
@@ -18,10 +18,10 @@ from branca.colormap import LinearColormap
 import mapp5
 
 #github로부터 데이터 파일을 읽어오기 위한 raw url
-url_bar = 'https://raw.githubusercontent.com/ikdkd11/dashboard/main/python-for-realestate-data_bar-main/0_data_bar/streamlit_data_bar/result_data_bar1.csv'
+urlbar = 'https://raw.githubusercontent.com/ikdkd11/dashboard/main/python-for-realestate-data_bar-main/0_data_bar/streamlit_data_bar/result_data_bar1.csv'
 
 # 데이터 불러오기
-data_bar = pd.read_csv(url_bar)
+data_bar = pd.read_csv(urlbar)
 
 # 필요없는 열 제거
 data_bar = data_bar.drop(['행정구역', '이름'], axis=1)
