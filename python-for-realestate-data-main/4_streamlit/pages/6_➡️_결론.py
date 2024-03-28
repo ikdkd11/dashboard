f9042936826fc005e2ca0afad2d023f4a1e0f6d1
ikdkd11 <kdkime86@gmail.com>
2024-03-28 10:17:21 +0900

    Update 6_➡️_결론.py

--- python-for-realestate-data-main/4_streamlit/pages/6_➡️_결론.py
+++ python-for-realestate-data-main/4_streamlit/pages/6_➡️_결론.py
@@ -43,6 +43,18 @@ st.markdown("""
     """, unsafe_allow_html=True)
 st.plotly_chart(bar1r, use_container_width=True)
 st.table(df1)
+st.markdown("""
+    <style>
+    .big-font {
+        font-size:20px !important;
+    }
+    </style>
+    <div class="big-font">
+    ○ 육상교각의 노면온도가 수상교각의 노면온도 감소량보다 0.5~0.8℃ 가량 더 크게 감소하는 것으로 나타났다.<br>
+    ○ 노면온도가 더 크게 감소하는 특성과 더불어 주변 지리환경에 따라 커브, 내리막 등 운전 상의 난점이 존재하는 육상교각의 특성을 고려했을 때 육상교각의 결빙사고 위험성이 높다고 판단됨<br>
+    3. 터널의 경우, 터널의 길이가 길수록 터널 출입구 주변의 노면온도가 크게 감소
+    </div>
+    """, unsafe_allow_html=True)
 st.subheader('1차 관측 위험구간')
 st.markdown('')
 st.markdown('위험구간 100%가 교각이나 터널 출입구에 해당하였고, 이에 맞춰 위험구간을 1.육상교각, 2. 수상교각, 3. 터널 출입구 세 가지로 분류하였음')
