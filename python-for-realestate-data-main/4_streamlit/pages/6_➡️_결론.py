6a86b002527f95034dce874f43025efca3eff296
ikdkd11 <kdkime86@gmail.com>
2024-03-28 09:59:41 +0900

    Update 6_➡️_결론.py

--- python-for-realestate-data-main/4_streamlit/pages/6_➡️_결론.py
+++ python-for-realestate-data-main/4_streamlit/pages/6_➡️_결론.py
@@ -20,7 +20,7 @@ import result1
 
 bar1r = result1.bar1
 
-st.header('분석 결과1: 교각, 터널마다 다르게 나타나는 노면온도 하락 패턴, divider='rainbow'')
+st.header('분석 결과1: 교각, 터널마다 다르게 나타나는 노면온도 하락 패턴', divider='rainbow')
 st.markdown('- 교각, 터널에서만 노면온도가 짧은 시간(수 초 이내)동안에 급락(1℃ 이상 하강) - 교각의 경우, 육상교각에서 수상교각보다 노면온도가 크게 감소- 터널의 경우, 터널의 길이가 길수록 터널 출입구 주변의 노면온도가 크게 감소')
 st.subheader('1차 관측 위험구간')
 st.markdown('')
