ac49f224407d97354848c386edc5eabaea932916
ikdkd11 <kdkime86@gmail.com>
2024-04-16 13:00:14 +0900

    Update 1_❄️_초당교차로.py

--- python-for-realestate-data-main/4_streamlit/pages/1_❄️_초당교차로.py
+++ python-for-realestate-data-main/4_streamlit/pages/1_❄️_초당교차로.py
@@ -27,6 +27,23 @@ table1 = tta.average_temperatures1
 grph1 = tot3.grp11
 grbp11 = tta.grbp1
 st.header('위험구간 첫번째 - 초당교차로', divider='rainbow')    
+# CSS를 이용하여 우측 상단에 텍스트를 고정시키는 HTML 코드
+css_style = """
+<style>
+.fixed-top-right {
+    position: fixed;
+    top: 10px;
+    right: 10px;
+    background-color: transparent;
+    padding: 8px;
+    z-index: 999;
+}
+</style>
+<div class="fixed-top-right">광주지방기상청 관측과 제공</div>
+"""
+
+# 스트림릿 페이지에 HTML 컴포넌트 추가
+html(css_style)
 st.subheader('<초당교차로 - 초당교>')
 st.write('주소: 전라남도 보성군 보성읍 옥평리 952-8')
 col1, col2 = st.columns([1,1])
